6df6ce234c327e09f300f129ae24dd8e0d4e1785
Jorge_dany <spumasc11@gmail.com>
2026-09-05 23:46:24 -0600

    SO subject

---
+++ Assets/ScriptableObjects/SubjectToTest.cs
@@ -0,0 +1,17 @@
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "SubjectToTest", menuName = "Game/SubjectToTest")]
+public class SubjectToTest : ScriptableObject
+{
+    public string subjectID;
+    public string subjectHead;
+    public string subjectBody;
+    public void randomizeID()
+    {
+        //Creas la funcionShige plox
+    }
+    public void randomizeHead()
+    {
+        //tu puedes Shige
+    }
+}
