452c90d678c4e5727e8f9801df318e763e9c789b
Raúl Sánchez <shigelorsh@gmail.com>
2026-09-06 18:51:31 -0600

    Minigames Data

--- Assets/ScriptableObjects/MinigamesData.cs
+++ Assets/ScriptableObjects/MinigamesData.cs
@@ -3,6 +3,9 @@ using UnityEngine;
 [CreateAssetMenu(fileName = "MinigamesData", menuName = "Game/MinigamesData")]
 public class MinigamesData : ScriptableObject
 {
-    
+    public string simonSays;
+    public int dialValue;
+    public int sliderValue;
 
+    
 }
