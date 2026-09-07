d643c2f71929a0d137dc1cb7592e16090d232602
Pab1401 <pab140.ssb@gmail.com>
2026-09-06 20:37:15 -0600

    How I hate you, Pivote

--- Assets/Scripts/Sliders/p_moveSlider.cs
+++ Assets/Scripts/Sliders/p_moveSlider.cs
@@ -8,8 +8,9 @@ using UnityEngine.InputSystem;
 public class p_moveSlider : MonoBehaviour, p_ISlider
 {   
     public int currentPosition = 0;
-    public int maxPosition = 6;
+    public const int maxPosition = 2; // 0 = min, 1 = mid, 2 = max
     public Transform min;
+    public Transform mid;
     public Transform max;
     [SerializeField] private Transform slide;
     public Vector3[] position;
@@ -17,19 +18,26 @@ public class p_moveSlider : MonoBehaviour, p_ISlider
 
     void Start()
     {
-        // gameObject.transform.position = position[0];
+        position = new Vector3[]
+        {
+            min.position,
+            mid.position,
+            max.position,
+        };
+
+        slide.position = position[0];
+        finalPosition = slide.position;
     }
 
     void ShouldMove(Vector3 target)
     {
-        slide.position = new Vector3 (slide.transform.position.x, slide.transform.position.y, (target.z < min.position.z? min.position.z : target.z) > max.position.z? max.position.z : target.z);
+        currentPosition = currentPosition + 1 > maxPosition ? 0 : currentPosition + 1;
+        slide.position = position[currentPosition];
         finalPosition = slide.position;
-        Debug.Log(finalPosition);
     }
 
     void p_ISlider.ShouldMove(Vector3 target)
     {
-        Debug.Log("hit" + target);
         ShouldMove(target);
     }
 }
